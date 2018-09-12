2ebec78da2463abb6e80145d3c11f82b4bf8dcb6
Phway Thant Chae <phwaythantchel@zwenex.com>
2018-09-12 18:20:22 +0630

    chapter 11 Account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users =  User.paginate(page: params[:page],:per_page => 10)
+    @users = User.where(activated: true).paginate(page: params[:page],:per_page => 10)
   end
 
   def new
@@ -14,15 +14,19 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      remember @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+      # log_in @user
+      # remember @user
+      # flash[:success] = "Welcome to the Sample App!"
+      # redirect_to @user
     else 
       render 'new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
 
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :activation_token
 
-    before_save { email.downcase! }
+    before_create :create_activation_digest
+    before_save :downcase_email
     validates :name, presence: true, length: { maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
@@ -23,18 +24,37 @@ class User < ApplicationRecord
         BCrypt::Password.create(string, cost: cost)
     end
 
+    def downcase_email
+        self.email = email.downcase
+    end
+
     # Returns a random token.
     def self.new_token
         SecureRandom.urlsafe_base64
     end
 
-    def authenticated?(remember_token)
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    def authenticated?(attribute, token)
+        digest = send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(remember_token)
         # same as self.remember_digest
     end
 
     def forget
         update_attribute(:remember_digest, nil)
     end
+
+    def create_activation_digest
+        self.activation_token = User.new_token
+        self.activation_digest = User.digest(activation_token)
+    end
+
+    def activation
+        update_columns(activated: true, activated_at: Time.zone.now)
+    end
+
+    def send_activation_email
+        UserMailer.account_activation(self).deliver_now
+    end
 end
+
