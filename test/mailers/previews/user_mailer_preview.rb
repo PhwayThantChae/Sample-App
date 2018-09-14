22583b759bae058e0b72669a7ec5df405ef46e47
Phway Thant Chae <phwaythantchel@zwenex.com>
2018-09-14 18:00:22 +0630

    Chapter 12 Password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -5,13 +5,15 @@ class UserMailerPreview < ActionMailer::Preview
   def account_activation
     user = User.find_by(email: "phway1@gmail.com")
     user.activation_token = User.new_token
-    user.activation_digest = User.digest(user.activation_token)
+    # user.activation_digest = User.digest(user.activation_token)
     UserMailer.account_activation(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.find_by(email: "example-3@railstutorial.org")
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 
 end
